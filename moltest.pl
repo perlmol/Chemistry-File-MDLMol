889569b083338070c1bdc6c8602d7548094c957e
Andrius Merkys <andrius.merkys@gmail.com>
2021-04-10 09:24:25 +0300

    Cleaned up moltest.pl.

--- moltest.pl
+++ moltest.pl
@@ -1,19 +1,9 @@
-#!/home/ivan/bin/perl
+#!/usr/bin/perl
 
-use warnings;
 use strict;
-use blib;
-#use lib '../Mol/blib/lib';
+use warnings;
+
 use Chemistry::File::MDLMol;
-#use Chemistry::File::SDF;
 
 my $mol = Chemistry::Mol->read("test.mol");
-#print $mol->print;
 print $mol->print(format => "mdl");
-
-#my @mols = Chemistry::Mol->read("test_data.sdf");
-#print $mols[0]->print;
-
-#for my $mol (@mols) {
-#    print $mol->name, "\t", $mol->attr("sdf/<PKA>")||'', "\t", $mol->mass, "\n";
-#}
